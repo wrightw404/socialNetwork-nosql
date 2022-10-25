4ac28d4bfe44ddf717f2582076be1b1a41bbb9b0
Will <wrightw404@gmail.com>
2022-10-24 20:54:40 -0700

    fixed regex for email validation in user model

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new Schema ({
             type: String,
             required: true,
             unique: true,
-            match: [/.+@|\..+/, "Must match an email address"]
+            match: [/.+@.+\..+/, "Must match an email address"]
         },
     thoughts: [
         {
