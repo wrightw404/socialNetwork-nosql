f522a943ce2dde791465508e3c87cb9315fbed46
Will <wrightw404@gmail.com>
2022-10-24 20:47:44 -0700

    created user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema ({
+    username: {
+            type: String,
+            required: true,
+            unique: true, 
+            trim: true
+        },
+    email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@|\..+/, "Must match an email address"]
+        },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectID,
+            ref: "Thought",
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectID,
+            ref: "User",
+        }
+    ]
+},
+{
+    toJSON: {
+            virtuals: true,
+        }, 
+            id: false,
+}
+)
