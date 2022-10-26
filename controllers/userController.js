ad595922526dbedd29499477f8f2ca3838dd2ee7
Will <wrightw404@gmail.com>
2022-10-25 17:15:47 -0700

    created getFriend and deleteFriend

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,6 +56,33 @@ module.exports = {
           )
           .catch((err) => res.status(500).json(err));
       },
+
+      getFriendId (req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: {friends: req.params.friendId}},
+            { new: true}
+          )
+            .then((user) =>
+              !user
+                ? res.status(404).json({ message: 'No friend with this id!' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+        },
+
+        deleteFriend (req, res) {
+        User.findOneAndDelete({  _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+            )
+            .then((user) =>
+              !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+          )
+            .catch((err) => res.status(500).json(err));
+        }
     
 
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
-const {getUsers, createUser, getSingleUser, deleteUser, updateUser} = require('../../controllers/userController')
+const {getUsers, createUser, getSingleUser, deleteUser, updateUser, getFriendId, deleteFriend} = require('../../controllers/userController')
 
 // api/users
 router.route('/').get(getUsers).post(createUser);
 
 // api/users/:userid
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(getFriendId).delete(deleteFriend)
 
 
 
